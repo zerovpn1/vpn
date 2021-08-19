853d9ec4cc476656a54768867fa3f638393e14f9
zerovpn1 <89145270+zerovpn1@users.noreply.github.com>
2021-08-19 15:48:15 +0700

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#         Premium Auto Script By ZeroVpn        #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 9 & Debian 10 64 bit               #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Build Up By ZEROVPN                           #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Thanks To                                     #"
+echo -e "#-----------------------------------------------#"
+echo -e "# ZeroVpn                                       #"
+echo -e "# Jesus Crisht                                  #"
+echo -e "# My Family                                     #"
+echo -e "# Google                                        #"
+echo -e "# Admin Portal & White Vps                      #"
+echo -e "# VPNPREMSTORE                                 #"
+echo -e "#                                               #"
+echo -e "#-----------------------------------------------#"
+echo -e "================================================="
